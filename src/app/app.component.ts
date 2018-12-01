d513cc414ff4f0d4fc154e20b3907eacc45d2f4e
Waqar <waqaryshaikh@gmail.com>
2018-12-01 09:54:27 +0530

    Adding APM files

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'pm-root',
+  template : `
+    <div><h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+  `
+})
+export class AppComponent {
+  title = 'Angular: Getting Started';
+  pageTitle:string = 'ACME product Management';
+}
