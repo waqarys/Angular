38350062286adde7c3a5621ccf94d8afabfc298b
Waqar <waqaryshaikh@gmail.com>
2018-12-02 16:38:03 +0530

    Navigation and Routing -> Guards

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   //selector: 'pm-product-detail',
@@ -11,9 +12,27 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    console.log('id: ',id);
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
